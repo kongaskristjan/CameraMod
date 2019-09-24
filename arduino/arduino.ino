08d1905e62182936fd2fa0d8994d32c8c0368675
Kristjan Kongas <kongaskristjan@gmail.com>
2019-09-24 23:48:55 +0300

    Add switching relay

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,8 +1,24 @@
+
+void plotDelay(int ms, int step=50) {
+  for(int t = 0; t < ms; t += step) {
+    delay(step);
+    
+    int sensorValue = analogRead(A1);
+    Serial.println(sensorValue);
+  }
+}
+
+
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int sensorValue = analogRead(A1);
-  Serial.println(sensorValue);
+  pinMode(A5, OUTPUT);
+  digitalWrite(A5, LOW);
+  plotDelay(15000);
+  
+  pinMode(A5, INPUT);
+  plotDelay(15000);
 }
