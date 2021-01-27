a886063a297c98b8b6e0b019b9d30b36bb7ad815
Kristjan Kongas <kongaskristjan@gmail.com>
2021-01-27 18:09:51 +0200

    Add camera detection mechanism, additional logging

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -4,6 +4,7 @@ const Mode mode = Mode::withReset;
 
 constexpr int delayLength = 50; // ms
 constexpr int totalIters = 15 /* seconds */ * (1000 /* ms per s */ / delayLength);
+constexpr int stabIters1 = 5, stabIters2 = 2;
 
 const int voltagePin = A5, cameraPin = A3;
 
@@ -11,6 +12,11 @@ short sensorValues[totalIters];
 int iter = 0;
 
 // Sensor value range: 0-1023 (0-5V)
+
+struct Step {
+  int iter = 0;
+  int delta = 0;
+};
 
 void reset() {
   Serial.println("Resetting camera:");
@@ -23,8 +29,29 @@ void reset() {
   iter = 0;
 }
 
-bool detectCameraFailure() {
-  return true;
+Step findMaxStep(int startIter, int stabIters) {
+  Step best;
+  for(int i = startIter; i < totalIters - stabIters; ++i) {
+    int delta = sensorValues[i + stabIters] - sensorValues[i];
+    if(delta < best.delta) {
+      best.iter = i;
+      best.delta = delta;
+    }
+  }
+  return best;
+}
+
+bool detectCameraSuccess() {
+  Step s1 = findMaxStep(0, stabIters1);
+  Serial.println(String("1. step: iter: ") + s1.iter + "\t delta: " + s1.delta);
+  Step s2 = findMaxStep(s1.iter + stabIters1, stabIters2);
+  Serial.println(String("2. step: iter: ") + s2.iter + "\t delta: " + s2.delta);
+  if(s2.delta < -20) {
+    Serial.println("Camera success");
+    return true;
+  }
+    Serial.println("Camera fail");
+  return false;
 }
 
 void setup() {
@@ -48,13 +75,13 @@ void loop() {
   }
 
   if(iter == totalIters) {
-    bool cameraFailed = detectCameraFailure();
-    if(cameraFailed) {
-      reset();
-    } else {
-      Serial.println("Camera successful: Looping forever");
-      // Loop forever
+    bool cameraSuccess = detectCameraSuccess();
+    if(cameraSuccess) {
+      Serial.println("Looping forever");
       while(true) delay(10000);
+    } else {
+      Serial.println("Resetting");
+      reset();
     }
   } else {
     delay(delayLength);
